887ee1a778b7b8def35d7aa08319adc9194aac7b
Nazarbek Baltabaev <nazarbek.baltabaev@gmail.com>
2026-07-27 14:57:22 +0500

    chore: refresh POS shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'restaurant-pos-shell-v14';
+const CACHE_NAME = 'restaurant-pos-shell-v15';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/pos-icon.svg'];
 
 function isCacheableRequest(request) {
